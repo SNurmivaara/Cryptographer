764724e5fc47344c092dc49f5d4900cfb9574970
SNurmivaara <sami.nurmivaara@helsinki.fi>
2017-04-25 12:52:19 +0300

    improved functionality

--- cryptographer/src/main/java/cryptographer/Main.java
+++ cryptographer/src/main/java/cryptographer/Main.java
@@ -21,30 +21,44 @@ public class Main {
     public static void main(String[] args) {
         Cipher cipher = new Cipher();
         String key = "";
-        int cipherType;
+        String cipherType;
 
-        System.out.print("Enter desired cipher\n1 for casear shift\n");
-        cipherType = scanner.nextInt();
-        
-        switch (cipherType) {
-            case 1:
-                System.out.println("Enter shift for caesar-cipher:");
-                key = scanner.next();
-                break;
+        System.out.print("Enter desired cipher\n1 for casear shift\ne for exit\n");
+        while (true) {
+
+            cipherType = scanner.next().toLowerCase();
+
+            switch (cipherType) {
+                case "1":
+                    System.out.println("Enter shift for caesar-cipher:");
+                    key = scanner.next();
+                    cipher.setKey(1, key);
+                    break;
+                case "e":
+                    return;
+            }
+
+            System.out.println("Enter 1 for ciphering or 2 for deciphering");
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                System.out.print("Enter text to cipher: ");
+                String input = scanner.next();
+                System.out.println(input + " => " + cipher.cipher(input));
+            } else {
+                System.out.println("Enter text to decipher: ");
+                String input = scanner.next();
+                System.out.println(input + " => " + cipher.decipher(input) + "\n_________________________________");
+            }
+            System.out.print("\nEnter desired cipher\n1 for casear shift\ne for exit\n");
         }
-        cipher.setKey(cipherType, key);
-        
-        System.out.print("Enter string to cipher: ");
-        String input = scanner.next();
-        System.out.println(input + " => " + cipher.cipher(input));
+    }
 
-        //For testing purposes for now
+    /* For testing purposes for now
         System.out.println(
                 "Choose file");
         FileReader fr = new FileReader();
 
         System.out.println(fr.getFileContentsAsString());
-
-    }
-
+     */
 }
